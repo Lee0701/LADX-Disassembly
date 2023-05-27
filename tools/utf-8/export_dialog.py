9c33a72bb3061a74bc2c70871f5c4f70976103ba
Lee0701 <hgl1002@naver.com>
2023-05-28 06:15:31 +0900

    Add dialog end mark processing to export script, set yaml export style

--- tools/utf-8/export_dialog.py
+++ tools/utf-8/export_dialog.py
@@ -2,6 +2,11 @@
 import sys
 import yaml
 
+def convert_char(c):
+    if c == 0:
+        return ord('@')
+    return c
+
 def main(input_file, output_file):
     with open(input_file, 'r') as f:
         content = f.read()
@@ -15,12 +20,13 @@ def main(input_file, output_file):
         elif line.strip().startswith('db'):
             chars = line.strip()[2:].split(',')
             chars = [int(c.strip()[1:], 16) for c in chars]
+            chars = [convert_char(c) for c in chars]
             chars = bytearray(chars)
             result[current_label].append(chars.decode('utf-8'))
 
     result = {key: '\n'.join(value) for key, value in result.items()}
     with open(output_file, 'w') as f:
-        yaml.dump(result, f, allow_unicode=True)
+        yaml.dump(result, f, allow_unicode=True, default_style='|')
 
 if __name__ == '__main__':
     args = sys.argv[1:]
